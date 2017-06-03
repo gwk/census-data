c0a39ba33808058cb61ee6635a7040366642d2d4
George King <george.w.king@gmail.com>
2017-06-03 14:25:56 -0400

    use both sqrt and log scales for color.

--- acs-pop-dots.json.py
+++ acs-pop-dots.json.py
@@ -43,9 +43,6 @@ def main():
   zero_areas  = count_by_pred(dots, lambda d: d.area == 0)
   errL(f'zero pop: {zero_pops}; zero area: {zero_areas}')
 
-  dens_lin_scale = 2 / dens_min # want minimum observed density to have a log2 value of 1.
-  dens_log_scale = 1 / log2(dens_max * dens_lin_scale) # want max observed density to be 1 on the log2 scale.
-
   x_min = min(d.lon for d in dots)
   x_max = max(d.lon for d in dots)
   y_min = min(d.lat for d in dots)
@@ -71,22 +68,33 @@ def main():
 
   dots_json = []
 
+  dens_lin_scale = 2 / dens_min # want minimum observed density to have a log2 value of 1.
+  dens_log_scale = 1 / log2(dens_max * dens_lin_scale) # want max observed density to be 1 on the log2 scale.
+
   for dot in err_progress(dots):
     nx = (dot.lon - ox) / w
     ny = (h - (dot.lat - oy)) / w  # flip y, then normalize by w.
 
-    r = sqrt(dot.area / area_max)
+    radius = sqrt(dot.area / area_max)
 
     d = dot.pop_density
     if d == 0:
-      g = 0
+      r, g, b = 0, 0, 0
     else:
-      v = log2(d * dens_lin_scale) * dens_log_scale
-      g = int(round(255 * v))
-    color = f'#00{g:2x}00'
-    dots_json.append([nx, ny, r, color])
+      lin_v = d / dens_max
+      sqrt_v = sqrt(lin_v)
+      log_v = log2(d * dens_lin_scale) * dens_log_scale
+      r = pix(4 * sqrt_v)
+      g = pix(log_v)
+      b = r
+    color = f'#{r:02x}{g:02x}{b:02x}'
+    dots_json.append([nx, ny, radius, color])
 
   out_json(dots_json)
+
+
+def pix(normalized):
+  return int(round(255 * min(1.0, normalized)))
 
 
 class Dot(NamedTuple):
